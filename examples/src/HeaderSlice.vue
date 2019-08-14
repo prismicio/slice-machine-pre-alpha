445a1d1ae618aef677457e65adc69dbb6992262a
Hugo Villain <hugo.villain@prismic.io>
2019-08-14 17:00:21 +0200

    Added app to examples

---
+++ examples/src/HeaderSlice.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>My example:</h2>
+    <header-slice title="hello">
+      <template v-slot:paragraph>
+        <p>
+          Salut les frèèères
+        </p>
+      </template>
+    </header-slice>
+  </div>
+</template>
+
+<script>
+import HeaderSlice from '../../src/HeaderSlice'
+
+export default {
+  name: 'HeaderSliceExample',
+  components: {
+    HeaderSlice
+  }
+}
+</script>
