98a1692bfe3b81a37e01283d123c33733934a7ba
Philip Snow <philipsnow@Philips-MacBook-Pro.local>
2019-11-15 18:19:26 +0100

    Committer: Philip Snow <philipsnow@Philips-MacBook-Pro.local> 	new file:   website/components/FooterMenu.vue 	new file:   website/components/Homepage/HomeBanner.vue 	new file:   website/components/Homepage/HomeLib.vue 	new file:   website/components/Homepage/HomeTitle.vue 	new file:   website/components/Homepage/ProsBlock.vue 	new file:   website/components/Homepage/TextGraphic.vue 	new file:   website/components/Homepage/VideoBlock.vue 	new file:   website/components/MainMenu.vue 	new file:   website/components/Menu/Burger.vue 	new file:   website/components/Menu/Sidebar.vue 	new file:   website/pages/component-library.vue 	new file:   website/static/clipboard.png 	new file:   website/store/sidemenu.js 	new file:   website/style/_breakpoints.scss 	new file:   website/style/_colors.scss 	new file:   website/style/_global.scss 	new file:   website/style/_mixins.scss 	new file:   website/style/_typography.scss

---
+++ website/components/Homepage/ProsBlock.vue
@@ -0,0 +1,149 @@
+<template>
+  <section class="box">
+    <h3 class="adv-title">
+      {{ $prismic.asText(slice.primary.slice_table_title) }}
+    </h3>
+    <div class="wrapper">
+      <section
+        v-for="(item, index) in slice.items"
+        :key="'item-' + index"
+        class="grid-items"
+      >
+        <prismic-image :field="item.row_image" />
+        <section class="adv-copy">
+          <h4>{{ $prismic.asText(item.row_title) }}</h4>
+          <p class="home_body_text">
+            {{ $prismic.asText(item.row_description) }}
+          </p>
+        </section>
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ProsBlock',
+  props: {
+    slice: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../style/_global';
+
+.box {
+  border: 1px solid #000000;
+  .adv-title {
+    padding: 15px 0;
+    text-align: center;
+  }
+}
+
+.wrapper {
+  border-top: 1px solid #000000;
+  padding: 20px;
+
+  * {
+    width: 100%;
+
+    img {
+      width: 95px;
+    }
+  }
+
+  .grid-items {
+    display: flex;
+    justify-content: center;
+    padding: 20px 0;
+    border-bottom: 1px solid #000000;
+
+    &:first-child {
+      padding-top: 0;
+    }
+
+    &:last-child {
+      padding-bottom: 0;
+      border-bottom: 0;
+    }
+
+    img {
+      float: left;
+      width: 95px;
+      height: 95px;
+    }
+    .adv-copy {
+      padding-left: 10px;
+      h3 {
+        font-size: 16px;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 16px;
+      }
+    }
+  }
+}
+
+@include lg {
+  .wrapper {
+    display: grid;
+    grid-template-columns: repeat(1fr);
+    border-top: 1px solid #000000;
+
+    .grid-items {
+      .adv-copy {
+        padding-left: 0;
+      }
+      &:nth-child(1) {
+        grid-column: 1 / 2;
+        border-right: 1px solid #000000;
+        padding: 0 20px 20px 0;
+        img {
+          margin: 0 20px 0 0;
+        }
+      }
+      &:nth-child(2) {
+        grid-column: 2 / 2;
+        padding: 0 0 20px 0;
+        img {
+          margin: 0 20px 20px 20px;
+        }
+      }
+      &:nth-child(3) {
+        grid-column: 1 / 2;
+        border-right: 1px solid #000000;
+        margin: 0 0 20px 0;
+        padding: 20px 20px 0 0;
+        border-bottom: 0;
+        img {
+          margin: 0 20px 20px 0;
+        }
+      }
+      &:nth-child(4) {
+        grid-column: 2 / 2;
+        margin: 0 0 20px 0;
+        padding: 20px 0 0 0;
+        border-bottom: 0;
+        img {
+          margin: 0 20px 20px 20px;
+        }
+      }
+      &:nth-child(5) {
+        border-top: 1px solid #000000;
+        img {
+          margin: 0 20px 0 0;
+        }
+      }
+      &:last-child {
+        padding: 20px 0 0 0;
+        grid-column: 1 /4;
+      }
+    }
+  }
+}
+</style>
---
+++ website/pages/component-library.vue
@@ -0,0 +1,103 @@
+<template>
+  <main>
+    <MainMenu :menu="menuContent" />
+    <Body class="body--white">
+      <h3 class="card-section-title">
+        {{ $prismic.asText(pageContent.page_title) }}
+      </h3>
+      <p class="card-section-description">
+        {{ $prismic.asText(pageContent.page_description) }}
+      </p>
+      <row class="homerows">
+        <Card v-for="card in lst" :key="card.displayName" v-bind="card" />
+      </row>
+    </Body>
+    <FooterMenu :menu="menuContent" />
+  </main>
+</template>
+
+<script>
+import Prismic from 'prismic-javascript'
+import PrismicConfig from '~/prismic.config.js'
+import MainMenu from '@/components/MainMenu'
+import FooterMenu from '@/components/FooterMenu'
+
+import Slices from '@/../src'
+import { createSlice } from '~/utils'
+import Card from '@/components/Card'
+import Body from '@/components/Body'
+import Row from '@/components/Row'
+
+const lst = Object.keys(Slices)
+  .map(createSlice)
+  .filter(e => e) // eslint-disable-line
+
+export default {
+  name: 'HomeLib',
+  components: {
+    ...Slices,
+    Card,
+    Body,
+    Row,
+    MainMenu,
+    FooterMenu
+  },
+  data: () => ({
+    lst
+  }),
+  async asyncData({ params, error, req }) {
+    try {
+      // Fetching the API object
+      const api = await Prismic.getApi(PrismicConfig.apiEndpoint, { req })
+
+      // Query to get the page content
+      let pageContent = {}
+      const page = await api.getByUID('page', 'component-library')
+      pageContent = page.data
+
+      // Query to get the menu content
+      let menuContent = {}
+      const menu = await api.getByUID('menu', 'main_menu')
+      menuContent = menu.data
+
+      // Load the edit button
+      if (process.client) window.prismic.setupEditButton()
+
+      return {
+        // Page
+        pageContent,
+        // Menu
+        menuContent
+      }
+    } catch (e) {
+      error({ statusCode: 404, message: 'Page not found' })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/_global';
+
+.homerows {
+  padding: 25px 0;
+}
+
+.card-section {
+  margin: 0 auto;
+  &-description {
+    margin: 0 auto;
+    font-size: 20px;
+    line-height: 34px;
+    font-family: $base-font-primary;
+  }
+  .button {
+    margin: 0 auto;
+    width: 250px;
+    height: 70px;
+    font-size: 20px;
+    line-height: 24px;
+    display: grid;
+  }
+}
+</style>
