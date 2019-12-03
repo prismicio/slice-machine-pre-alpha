52cb9a73afcf18c00535dd0dd6218a3dfae97b86
Hugo Villain <hugo.villain@prismic.io>
2019-12-03 15:29:08 +0100

    Fix Fractal build config

--- sandbox/fractal.js
+++ sandbox/fractal.js
@@ -1,56 +1,32 @@
 'use strict'
 
-/*
- * Require the path module
- */
 const path = require('path')
-
-/*
- * Require the Fractal module
- */
+const mandelbrot = require('@frctl/mandelbrot')
 const fractal = (module.exports = require('@frctl/fractal').create())
 
-/*
- * Give your project a title.
- */
 fractal.set('project.title', 'Prismic Slice-Machine')
 
-/*
- * Tell Fractal where to look for components.
- */
 fractal.components.set('path', path.join(__dirname, '/src/components'))
-
-/*
- * Tell Fractal where to look for documentation pages.
- */
 fractal.docs.set('path', path.join(__dirname, '/src/docs'))
 
-/*
- * Tell the Fractal web preview plugin where to look for static assets.
- */
 fractal.web.set('static.path', path.join(__dirname, 'public'))
-fractal.web.set('static.path', __dirname + '/public');
-
-fractal.web.set('builder.dest', path.join(__dirname, 'dist'))
 
 /* =========================================
    Theming
 =========================================== */
 
-// create a new instance with custom config options
-// all the paths inside this are relative to the theme's root folder `theme` locted in the project's source folder
-const mandelbrot = require('@frctl/mandelbrot')
 const jTheme = mandelbrot({
-    // theme config here
-    favicon: '/assets/favicon.ico',
-    skin: 'white',
-    nav: ['docs', 'components'], // show docs above components in the sidebar
-    panels: ['notes', 'html', 'resources', 'info'], // possible values: "html", "view", "context", "resources", "info", "notes"
-    styles: ['default', '/css/theme.css']
+  favicon: '/assets/favicon.ico',
+  skin: 'white',
+  nav: ['docs', 'components'],
+  panels: ['notes', 'html', 'resources', 'info'],
+  styles: ['default', '/theme/css/theme.css']
 })
 
-// specify a directory to hold the theme override templates
-// jTheme.addLoadPath(__dirname + "/theme");
-jTheme.addLoadPath(__dirname + '/theme/views')
-jTheme.addStatic(__dirname + '/theme')
-fractal.web.theme(jTheme) // tell Fractal to use the configured theme by default
+fractal.web.theme(jTheme)
+
+jTheme.addLoadPath(path.join(__dirname, '/theme'))
+jTheme.addLoadPath(path.join(__dirname, '/theme/views'))
+jTheme.addStatic(path.join(__dirname, 'theme'), '/theme')
+
+fractal.web.set('builder.dest', path.join(__dirname, 'dist'))
--- sandbox/gulpfile.js
+++ sandbox/gulpfile.js
@@ -16,104 +16,89 @@ const browserSync = require('browser-sync').create()
 const reload = browserSync.reload
 const merge = require('merge-stream')
 const path = require('path')
-const image = require("gulp-image")
+const image = require('gulp-image')
 
-gulp.task('browserSync', function () {
-    browserSync.init({
-        files: ['src/**/*.*'],
-        proxy: 'localhost:7000', // fractal uses localhost:7000
-        injectChanges: true
-    })
+gulp.task('browserSync', function() {
+  browserSync.init({
+    files: ['src/**/*.*'],
+    proxy: 'localhost:7000', // fractal uses localhost:7000
+    injectChanges: true
+  })
 })
 
-gulp.task("images", function () {
-    return gulp
-        .src(["src/assets/img/**/*"])
-        .pipe(image())
-        .pipe(gulp.dest("public/img"));
+gulp.task('images', function() {
+  return gulp
+    .src(['src/assets/img/**/*'])
+    .pipe(image())
+    .pipe(gulp.dest('public/img'))
 })
 
-gulp.task('scripts', function () {
-    return (
-        gulp
-            .src(["src/assets/js/*.js", "src/components/**/*.js"])
-            // .src(['src/**/*.js', 'src/components/**/*.js'])
-            .pipe(
-                babel({
-                    presets: ['@babel/env']
-                })
-            )
-            // .pipe(concat("main.js"))
-            .pipe(sourcemaps.write('.'))
-            .pipe(gulp.dest('public/js'))
-            .pipe(reload({ stream: true }))
-    )
-})
-
-gulp.task('sass', function () {
-    return gulp
-        .src(['src/assets/scss/**/*.scss', 'src/components/**/*.scss'])
-        .pipe(sassGlob())
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css'))
-        .pipe(reload({ stream: true }))
-})
-
-gulp.task('watch', function () {
-    browserSync.init({
-        files: ['src/**/*.*'],
-        proxy: 'localhost:7000', // fractal uses localhost:7000
-        injectChanges: true
-    })
-
-    gulp.watch(
-        ['src/assets/scss/**/*.scss', 'src/components/**/*.scss'],
-        gulp.series('sass')
-    )
-    gulp.watch(
-        ['src/assets/js/**/*.js', 'src/components/**/*.js'],
-        gulp.series('scripts')
-    )
-
-    gulp.watch(["src/assets/img/**/*"], gulp.series("images"))
-
-    // Reloads the browser whenever HTML or JS files change
-    gulp.watch('src/*.hbs').on('change', browserSync.reload)
-})
-
-function customPlumber(errTitle) {
-    return plumber({
-        errorHandler: notify.onError({
-            title: errTitle || 'Error running Gulp',
-            message: 'Error: <%= error.message %>'
+gulp.task('scripts', function() {
+  return (
+    gulp
+      .src(['src/assets/js/*.js', 'src/components/**/*.js'])
+      // .src(['src/**/*.js', 'src/components/**/*.js'])
+      .pipe(
+        babel({
+          presets: ['@babel/env']
         })
-    })
-}
-
-gulp.task('fractal:start', function () {
-    const server = fractal.web.server({
-        sync: true
-    })
-    server.on('error', err => logger.error(err.message))
-    return server.start().then(() => {
-        logger.success(`Fractal server is now running at ${server.url}`)
-    })
+      )
+      // .pipe(concat("main.js"))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('public/js'))
+      .pipe(reload({ stream: true }))
+  )
 })
 
-/**
- * Currently unused due to bug:
- * "path" argument must be of type string. Received undefined
- * for builder dest argument.
- */
+gulp.task('sass', function() {
+  return gulp
+    .src(['src/assets/scss/**/*.scss', 'src/components/**/*.scss'])
+    .pipe(sassGlob())
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(reload({ stream: true }))
+})
 
-gulp.task('fractal:build', function () {
-    const builder = fractal.web.builder();
-    builder.on('progress', (completed, total) => logger.update(`Exported ${completed} of ${total} items`, 'info'));
-    builder.on('error', err => logger.error(err.message));
-    return builder.build().then(() => {
-        logger.success('Fractal build completed!');
-    });
-});
+gulp.task('watch', function() {
+  browserSync.init({
+    files: ['src/**/*.*'],
+    proxy: 'localhost:7000', // fractal uses localhost:7000
+    injectChanges: true
+  })
+
+  gulp.watch(
+    ['src/assets/scss/**/*.scss', 'src/components/**/*.scss'],
+    gulp.series('sass')
+  )
+  gulp.watch(
+    ['src/assets/js/**/*.js', 'src/components/**/*.js'],
+    gulp.series('scripts')
+  )
+
+  gulp.watch(['src/assets/img/**/*'], gulp.series('images'))
+
+  // Reloads the browser whenever HTML or JS files change
+  gulp.watch('src/*.hbs').on('change', browserSync.reload)
+})
+
+gulp.task('fractal:start', function() {
+  const server = fractal.web.server({
+    sync: true
+  })
+  server.on('error', err => logger.error(err.message))
+  return server.start().then(() => {
+    logger.success(`Fractal server is now running at ${server.url}`)
+  })
+})
+
+gulp.task('build', async function() {
+  const builder = fractal.web.builder()
+  builder.on('progress', (completed, total) =>
+    logger.update(`Exported ${completed} of ${total} items`, 'info')
+  )
+  builder.on('error', err => logger.error(err.message))
+  await builder.build()
+  logger.success('Fractal build completed!')
+})
 
 gulp.task('default', gulp.series('fractal:start', 'watch'))
-// gulp.task("default", gulp.series("watch"));
