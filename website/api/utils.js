9aa785ca7995bc7e9e443d6040d9f29f12ade85b
Hugo Villain <hugo.villain@prismic.io>
2019-12-19 15:29:53 +0100

    Hotfix api bundler

--- website/api/utils.js
+++ website/api/utils.js
@@ -8,27 +8,27 @@ const prod = () => process.env.NODE_ENV === 'production'
 const isDirectory = source => lstatSync(source).isDirectory()
 
 const sliceFolders = {
-  nuxt: path.join(process.cwd(), 'src/slices')
+	nuxt: path.join(process.cwd(), 'src/slices')
 }
 
 const getDirectories = source =>
-  readdirSync(source)
-    .map(name => path.join(source, name))
-    .filter(isDirectory)
+	readdirSync(source)
+		.map(name => path.join(source, name))
+		.filter(isDirectory)
 
 const readCustomTypes = folder => {
-  if (folder) {
-    const customTypesPath = path.join(folder, 'index.json')
-    // test path maybe?
-    const customTypes = JSON.parse(fs.readFileSync(customTypesPath, 'utf8'))
-    customTypes.forEach(t => {
-      const customType = t
-      const valuePath = path.join(folder, customType.value)
-      customType.value = JSON.parse(fs.readFileSync(valuePath, 'utf8'))
-    })
-    return customTypes
-  }
-  return null
+	if (folder) {
+		const customTypesPath = path.join(folder, 'index.json')
+		// test path maybe?
+		const customTypes = JSON.parse(fs.readFileSync(customTypesPath, 'utf8'))
+		customTypes.forEach(t => {
+			const customType = t
+			const valuePath = path.join(folder, customType.value)
+			customType.value = JSON.parse(fs.readFileSync(valuePath, 'utf8'))
+		})
+		return customTypes
+	}
+	return null
 }
 
 const hyphenateRE = /\B([A-Z])/g
@@ -40,58 +40,60 @@ const hyphenate = str => str.replace(hyphenateRE, '-$1').toLowerCase()
  * @return {Object}           An object with snake cased `sliceName`
  */
 const getModelFromSliceName = (sliceName, url = sliceFolders.nuxt) => {
-  try {
-    const component = parse(path.join(`${url}/${sliceName}/index.vue`))
-    const model = JSON.parse(
-      fs.readFileSync(path.join(`${url}/${sliceName}/model.json`), 'utf8')
-    )
+	try {
+		const component = parse(path.join(`${url}/${sliceName}/index.vue`))
+		const model = JSON.parse(
+			fs.readFileSync(path.join(`${url}/${sliceName}/model.json`), 'utf8')
+		)
 
-    const key = hyphenate(component.displayName).replace(/-/g, '_')
-    return [key, model]
-  } catch (e) {}
+		const key = hyphenate(component.displayName).replace(/-/g, '_')
+		return [key, model]
+	} catch (e) {}
 }
 
 const getAllFromSliceName = (sliceName, url = sliceFolders.nuxt) => {
-  try {
-    const [key, model] = getModelFromSliceName(sliceName, url)
-    const meta = JSON.parse(
-      fs.readFileSync(`${url}/${sliceName}/meta.json`, 'utf8')
-    )
-    return {
-      key,
-      model,
-      meta
-    }
-  } catch (e) {}
+	try {
+		const [key, model] = getModelFromSliceName(sliceName, url)
+		const meta = JSON.parse(
+			fs.readFileSync(`${url}/${sliceName}/meta.json`, 'utf8')
+		)
+		return {
+			key,
+			model,
+			meta
+		}
+	} catch (e) {}
 }
 
 const zipFile = (zip, pathFrom, pathTo) => {
-  try {
-    const f = fs.readFileSync(pathFrom, 'utf8')
-    zip.file(pathTo, f)
-  } catch (e) {
-    if (!prod()) {
-      console.error('error in api/utils.js[87]: ', e)
-    }
-  }
+	try {
+		const f = fs.readFileSync(pathFrom, 'utf8')
+		zip.file(pathTo, f)
+	} catch (e) {
+		if (!prod()) {
+			console.error('error in api/utils.js[87]: ', e)
+		}
+	}
 }
 
 // Change this when you allow users to select the slices they want
 const getSliceNames = (slicesParams, url = sliceFolders.nuxt) => {
-  const allSlices = getDirectories(url)
-  return allSlices.map(path => {
-    const spl = path.split('/')
-    return spl[spl.length - 1]
-  })
+	console.log('getSliceNames', url)
+	const allSlices = getDirectories(url)
+	console.log('allSlices -> ', allSlices)
+	return allSlices.map(path => {
+		const spl = path.split('/')
+		return spl[spl.length - 1]
+	})
 }
 
 module.exports = {
-  prod,
-  sliceFolders,
-  getDirectories,
-  readCustomTypes,
-  getModelFromSliceName,
-  getAllFromSliceName,
-  zipFile,
-  getSliceNames
+	prod,
+	sliceFolders,
+	getDirectories,
+	readCustomTypes,
+	getModelFromSliceName,
+	getAllFromSliceName,
+	zipFile,
+	getSliceNames
 }
