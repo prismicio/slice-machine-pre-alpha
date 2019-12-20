ee1213d798ac50c3ab57003edf420b1813b1b692
Hugo Villain <hugo.villain@prismic.io>
2019-12-20 15:43:50 +0100

    Hotfix: src slice imports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-export { default as CallToAction } from './slices/CallToAction'
-export { default as CustomerLogos } from './slices/CustomerLogos'
-export { default as FaqSection } from './slices/FaqSection'
-export { default as PricingTable } from './slices/PricingTable'
-export { default as VideoHighlights } from './slices/VideoHighlights'
+export * from './slices'
 
+// In final bundle, this will be elsewhere
 export { default as SliceZone } from './SliceZone'
