e39fba5277faae8aaa07c091c20eaf5bb9258437
Sara Soueidan <contact@sarasoueidan.com>
2020-01-11 20:40:42 +0200

    update carousel and add inert polyfill

--- sandbox/src/components/slider-carousel/slider-carousel.js
+++ sandbox/src/components/slider-carousel/slider-carousel.js
@@ -1,3 +1,28 @@
+
+/* inert polyfill 
+ * source: https://cdn.rawgit.com/GoogleChrome/inert-polyfill/v0.1.0/inert-polyfill.min.js
+ */
+window.addEventListener("load", function () {
+  function h(a, b, c) { if (0 > b) { if (a.previousElementSibling) { for (a = a.previousElementSibling; a.lastElementChild;)a = a.lastElementChild; return a } return a.parentElement } if (a != c && a.firstElementChild) return a.firstElementChild; for (; null != a;) { if (a.nextElementSibling) return a.nextElementSibling; a = a.parentElement } return null } function g(a) { for (; a && a !== document.documentElement;) { if (a.hasAttribute("inert")) return a; a = a.parentElement } return null } (function (a) {
+    var b = document.createElement("style");
+    b.type = "text/css"; b.styleSheet ? b.styleSheet.cssText = a : b.appendChild(document.createTextNode(a)); document.body.appendChild(b)
+  })("/*[inert]*/[inert]{position:relative!important;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;pointer-events:none}[inert]::before{content:'';display:block;position:absolute;top:0;left:0;right:0;bottom:0}"); var c = 0; document.addEventListener("keydown", function (a) { c = 9 === a.keyCode ? a.shiftKey ? -1 : 1 : 0 }); document.addEventListener("mousedown",
+    function () { c = 0 }); document.body.addEventListener("focus", function (a) {
+      var b = a.target, f = g(b); if (f) {
+        if (document.hasFocus() && 0 !== c) {
+          var d = document.activeElement, e = new KeyboardEvent("keydown", { keyCode: 9, which: 9, key: "Tab", code: "Tab", keyIdentifier: "U+0009", shiftKey: !!(0 > c), bubbles: !0 }); Object.defineProperty(e, "keyCode", { value: 9 }); document.activeElement.dispatchEvent(e); if (d != document.activeElement) return; for (d = f; ;) {
+            d = h(d, c, f); if (!d) break; a: {
+              e = b; if (!(0 > d.tabIndex) && (d.focus(), document.activeElement !== e)) {
+                e =
+                  !0; break a
+              } e = !1
+            } if (e) return
+          }
+        } b.blur(); a.preventDefault(); a.stopPropagation()
+      }
+    }, !0); document.addEventListener("click", function (a) { g(a.target) && (a.preventDefault(), a.stopPropagation()) }, !0)
+});
+
 "use strict";
 var util = {
   keyCodes: {
@@ -92,7 +117,7 @@ var util = {
         leftCounter = 0; // reset it
 
         handlePaddleButtonsState();
-        setTimeout(updateHelper, 300);
+        updateHelper();
         slideCards();
       }
 
@@ -135,11 +160,13 @@ var util = {
             // entry.target.style.opacity = "1";
             entry.target.classList.add('is-visible');
             entry.target.setAttribute('aria-hidden', 'false');
+            entry.target.removeAttribute('inert');
           }
           else {
             // entry.target.style.opacity = "0";
             entry.target.classList.remove('is-visible');
             entry.target.setAttribute('aria-hidden', 'true');
+            entry.target.setAttribute('inert', '');
           }
         });
       }
@@ -160,20 +187,22 @@ var util = {
     }
 
     var initHelper = function () {
-      setTimeout(function () { updateHelper(); }, 300);
+      updateHelper();
     }
 
     var updateHelper = function () {
-      let visibleItems = Array.from(el.querySelectorAll('.c-carousel__card.is-visible'));
-      let cardNumbers = [];
-      let helper = el.querySelector('.c-carousel__SRHelper');
+      setTimeout(function () {
+        let visibleItems = Array.from(el.querySelectorAll('.c-carousel__card.is-visible'));
+        let cardNumbers = [];
+        let helper = el.querySelector('.c-carousel__SRHelper');
 
-      visibleItems.forEach(item => {
-        let number = cards.indexOf(item);
-        cardNumbers.push(number + 1);
-      });
+        visibleItems.forEach(item => {
+          let number = cards.indexOf(item);
+          cardNumbers.push(number + 1);
+        });
 
-      helper.innerHTML = 'Showing carousel items ' + cardNumbers.toString() + ' of ' + cards.length;
+        helper.innerHTML = 'Showing carousel items ' + cardNumbers.toString() + ' of ' + cards.length;
+      }, 300);
     }
 
     var initPaddleNav = function () {
@@ -255,7 +284,7 @@ var util = {
       incrementRightCounter();
       decrementLeftCounter();
       slideCards();
-      setTimeout(updateHelper, 300);
+      updateHelper();
       handlePaddleButtonsState();
     }
 
@@ -263,7 +292,7 @@ var util = {
       decrementRightCounter();
       incrementLeftCounter();
       slideCards();
-      setTimeout(updateHelper, 300);
+      updateHelper();
       handlePaddleButtonsState();
     }
 
