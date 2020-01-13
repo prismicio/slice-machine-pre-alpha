4292aa22151fe2caa1a2cb4e88f2cb2ab80b5efc
Hugo Villain <hugo.villain@prismic.io>
2020-01-13 12:05:12 +0100

    auto-format

--- website/pages/examples/nuxt/PricingTable.vue
+++ website/pages/examples/nuxt/PricingTable.vue
@@ -1,17 +1,17 @@
 <template>
-  <pricing-table :slice="mockData" />
+	<pricing-table :slice="mockData" />
 </template>
 <script>
 import { PricingTable } from '@/../src'
 import mockData from '@/../src/slices/PricingTable/mock.json'
 export default {
-  components: {
-    PricingTable
-  },
-  data() {
-    return {
-      mockData
-    }
-  }
+	components: {
+		PricingTable
+	},
+	data() {
+		return {
+			mockData
+		}
+	}
 }
 </script>
