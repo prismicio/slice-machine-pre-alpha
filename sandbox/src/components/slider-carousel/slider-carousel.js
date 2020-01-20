95950d47da7aee776ec84b4e23c17596f0be629a
Sara Soueidan <contact@sarasoueidan.com>
2020-01-20 15:59:35 +0200

    add prepend polyfill for IE11 testing

--- sandbox/src/components/slider-carousel/slider-carousel.js
+++ sandbox/src/components/slider-carousel/slider-carousel.js
@@ -1,6 +1,30 @@
 
 "use strict";
 
+(function (arr) {
+  arr.forEach(function (item) {
+    if (item.hasOwnProperty('prepend')) {
+      return;
+    }
+    Object.defineProperty(item, 'prepend', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function prepend() {
+        var argArr = Array.prototype.slice.call(arguments),
+          docFrag = document.createDocumentFragment();
+
+        argArr.forEach(function (argItem) {
+          var isNode = argItem instanceof Node;
+          docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+        });
+
+        this.insertBefore(docFrag, this.firstChild);
+      }
+    });
+  });
+})([Element.prototype, Document.prototype, DocumentFragment.prototype]);
+
 // Production steps of ECMA-262, Edition 6, 22.1.2.1
 if (!Array.from) {
   Array.from = (function () {
